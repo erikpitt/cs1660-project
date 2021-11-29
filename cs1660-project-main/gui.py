3158d66159bec3695bdd45f3afc7feda73312a18
erikpitt <60970413+erikpitt@users.noreply.github.com>
2021-11-28 21:28:44 -0500

    Updated gui.py to contain IP addresses

--- cs1660-project-main/gui.py
+++ cs1660-project-main/gui.py
@@ -1,35 +1,58 @@
 import tkinter as tk
+from tkinter.constants import END
+import sys
+
+IS_LOCAL = False
 
 def main():
+    global IS_LOCAL
+    
+    if len(sys.argv) > 1:
+        IS_LOCAL = sys.argv[1].lower() == 'true'
+
     window = tk.Tk()
     window.title('CS1660 Project 1')
-    window.geometry('250x120')
+    window.geometry('250x175')
 
-    hadoop_button = tk.Button(window, text='Apache Hadoop', command=open_hadoop)
+    text = tk.Text(window, height=8, state='disabled')
+
+    hadoop_button = tk.Button(window, text='Apache Hadoop', command=lambda: open_hadoop(text))
     hadoop_button.pack(side='top')
 
-    spark_button = tk.Button(window, text='Apache Spark', command=open_spark)
+    spark_button = tk.Button(window, text='Apache Spark', command=lambda: open_spark(text))
     spark_button.pack(side='top')
 
-    jupyter_button = tk.Button(window, text='Jupyter Notebook', command=open_jupyter)
+    jupyter_button = tk.Button(window, text='Jupyter Notebook', command=lambda: open_jupyter(text))
     jupyter_button.pack(side='top')
 
-    sonar_button = tk.Button(window, text='SonarQube and SonarScanner', command=open_sonar)
+    sonar_button = tk.Button(window, text='SonarQube and SonarScanner', command=lambda: open_sonar(text))
     sonar_button.pack(side='top')
+
+    text.pack(side='top')
 
     window.mainloop()
 
-def open_hadoop():
-    pass
+def write_text(text, link):
+    text['state'] = 'normal'
+    text.delete(1.0, END)
+    text.insert(1.0, link)
+    text['state'] = 'disabled'
 
-def open_spark():
-    pass
+def open_hadoop(text):
+    link = 'localhost:9870' if IS_LOCAL else '35.223.112.91:9870'
+    write_text(text, link)
 
-def open_jupyter():
-    pass
+def open_spark(text):
+    link = 'localhost:8080' if IS_LOCAL else '34.132.29.196:8080'
+    write_text(text, link)
 
-def open_sonar():
-    pass
+def open_jupyter(text):
+    link = 'localhost:8888' if IS_LOCAL else '34.136.84.126:8888'
+    write_text(text, link)
+
+def open_sonar(text):
+    link = 'localhost:9000' if IS_LOCAL else '104.197.67.245:9000'
+    write_text(text, link)
 
 if __name__ == '__main__':
     main()
